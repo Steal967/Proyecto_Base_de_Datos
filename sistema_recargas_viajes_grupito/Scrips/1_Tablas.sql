8e12598426c6d6805036c389f249ddeedac2ddaf
Steal967 <steven.steal96@gmail.com>
2025-05-26 18:52:21 -0500

    Create 1_Tablas.sql

---
+++ sistema_recargas_viajes_grupito/Scrips/1_Tablas.sql
@@ -0,0 +1,26 @@
+CREATE TABLE HistorialEstadosTarjeta (
+    id SERIAL PRIMARY KEY,
+    tarjeta_id INT REFERENCES TARJETAS(tarjeta_id),
+    estado_anterior VARCHAR(50),
+    estado_nuevo VARCHAR(50),
+    fecha_cambio DATE
+);
+
+CREATE TABLE Promociones (
+    promocion_id SERIAL PRIMARY KEY,
+    nombre VARCHAR(100),
+    descripcion TEXT,
+    tipo VARCHAR(50),
+    fecha_inicio DATE,
+    fecha_fin DATE
+);
+
+ALTER TABLE RECAEGAS ADD COLUMN promocion_id INT REFERENCES Promociones(promocion_id);
+
+CREATE TABLE Dispositivos (
+    dispositivo_id SERIAL PRIMARY KEY,
+    tipo VARCHAR(50), -- 'torniquete', 'movil', etc.
+    ubicacion TEXT
+);
+
+ALTER TABLE VIAJES ADD COLUMN dispositivo_id INT REFERENCES Dispositivos(dispositivo_id);
